f9bfd55a89068275cbdfb7df2a0dbb153adee196
PaulTheCheese <psangabriel52@gmail.com>
2024-11-06 11:08:10 +0800

    Add files via upload

--- web-js-project/Group Activity 1/process-signup.php
+++ web-js-project/Group Activity 1/process-signup.php
@@ -1,3 +1,86 @@
 <?php
-print_r($_POST);
+if (isset($_POST["submit"])){
+      $usernameSignup = $_POST["username"];
+      $email = $_POST["email"];
+      $password = $_POST["password"];
+      $confirm_password = $_POST["confirm_password"];
+
+      //Username Should be 3 to 30 Characters
+      if (strlen($usernameSignup) < 3 || strlen($usernameSignup) > 30){
+    echo("<script>
+             alert('Username must be 3 to 30 characters');
+             window.location.href = 'sign-up.html';
+          </script>");
+    exit;
+    }
+
+
+    //Password Should be 8 or more characters
+    if (strlen($password) < 8){
+        echo("<script>
+                 alert('Password must be at least 8 Characters');
+                 window.location.href = 'sign-up.html';
+              </script>");
+        exit;
+        }
+    
+
+    //Password Should contain at least 1 letter
+    if (! preg_match("/[a-z]/i", $password)){
+        echo("<script>
+                 alert('Password must contain at least one letter');
+                 window.location.href = 'sign-up.html';
+              </script>");
+        exit;
+        }
+
+
+    //Password Should contain at least 1 number
+    if (! preg_match("/[0-9]/", $password)){
+        echo("<script>
+                 alert('Password must contain at least one number');
+                 window.location.href = 'sign-up.html';
+              </script>");
+        exit;
+        }
+
+
+    //Password Should contain at least 1 number
+    if ($password !== $confirm_password){
+        echo("<script>
+                 alert('Passwords must match');
+                 window.location.href = 'sign-up.html';
+              </script>");
+        exit;
+        }
+
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    
+    $mysqli = require __DIR__ . "/database.php";
+    
+
+    //SQL Query for the Database
+    $sql = "INSERT INTO user (name, email, password_hash)
+            VALUES (?,?,?)";
+
+    $stmt = $mysqli-> stmt_init();
+    
+
+    //To check if the query has a syntax error
+    if (!$stmt->prepare($sql)){
+        die("SQL error: " . $mysql->error);
+    };
+    
+    $stmt->bind_param("sss",
+                       $usernameSignup,
+                       $email,
+                       $hashedPassword);
+
+   if( $stmt ->execute()){
+    echo("Sign Up Successful");
+   };
+    
+}
+
+
 ?>
